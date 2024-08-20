b6f3fef38b90eee853be461c81776828a59cf63c
verandaaa <s688922@gmail.com>
2024-08-20 21:36:39 +0900

    commit

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    {{ timezone }}<br/>
+    {{ dayjs().format() }}
+  </div>
+</template>
+
+<script setup>
+import dayjs from 'dayjs'
+const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone
+</script>
